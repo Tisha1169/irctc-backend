8e271610684ab12f72a5959c454023891776eb0b
Tisha1169 <tishadubey11@gmail.com>
2026-01-24 08:31:05 +0530

    Phase 4: JWT protected API implemented

--- irctc_backend/api/views.py
+++ irctc_backend/api/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer
 
 
@@ -20,3 +18,13 @@ class RegisterView(APIView):
             )
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProtectedTestView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "message": "You are authenticated!",
+            "user": request.user.username
+        })
